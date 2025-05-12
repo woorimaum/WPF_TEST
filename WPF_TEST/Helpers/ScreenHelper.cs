d7a2be4fc30c28dd6326c3c84db2d659c067311a
yh.choi <yh.choi@deepnoid.com>
2025-05-12 09:15:16 +0900

    NativeMethods 추가 font 추가

---
+++ WPF_TEST/Helpers/ScreenHelper.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace WPF_TEST.Helpers
+{
+    public class ScreenHelper
+    {
+        // References:
+        // http://referencesource.microsoft.com/#System.Windows.Forms/ndp/fx/src/winforms/Managed/System/WinForms/Screen.cs
+        // http://msdn.microsoft.com/en-us/library/windows/desktop/dd145072.aspx
+        // http://msdn.microsoft.com/en-us/library/windows/desktop/dd183314.aspx
+
+        /// <summary>
+        /// Indicates if we have more than one monitor.
+        /// </summary>
+        private static readonly bool MultiMonitorSupport;
+
+        // This identifier is just for us, so that we don't try to call the multimon
+        // functions if we just need the primary monitor... this is safer for
+        // non-multimon OSes.
+        private const int PRIMARY_MONITOR = unchecked((int)0xBAADF00D);
+
+        private const int MONITORINFOF_PRIMARY = 0x00000001;
+
+        /// <summary>
+        /// The monitor handle.
+        /// </summary>
+        private readonly IntPtr monitorHandle;
+
+        /// <summary>
+        /// Initializes static members of the <see cref="Screen"/> class.
+        /// </summary>
+        static ScreenHelper()
+        {
+            MultiMonitorSupport = NativeMethods.GetSystemMetrics(NativeMethods.SystemMetric.SM_CMONITORS) != 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Screen"/> class.
+        /// </summary>
+        /// <param name="monitor">The monitor.</param>
+        private ScreenHelper(IntPtr monitor)
+            : this(monitor, IntPtr.Zero)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Screen"/> class.
+        /// </summary>
+        /// <param name="monitor">The monitor.</param>
+        /// <param name="hdc">The hdc.</param>
+        private ScreenHelper(IntPtr monitor, IntPtr hdc)
+        {
+            if (NativeMethods.IsProcessDPIAware())
+            {
+                uint dpiX;
+
+                try
+                {
+                    if (monitor == (IntPtr)PRIMARY_MONITOR)
+                    {
+                        var ptr = NativeMethods.MonitorFromPoint(new NativeMethods.POINTSTRUCT(0, 0), NativeMethods.MonitorDefault.MONITOR_DEFAULTTOPRIMARY);
+                        NativeMethods.GetDpiForMonitor(ptr, NativeMethods.DpiType.EFFECTIVE, out dpiX, out _);
+                    }
+                    else
+                    {
+                        NativeMethods.GetDpiForMonitor(monitor, NativeMethods.DpiType.EFFECTIVE, out dpiX, out _);
+                    }
+                }
+                catch
+                {
+                    // Windows 7 fallback
+                    var hr = NativeMethods.D2D1CreateFactory(NativeMethods.D2D1_FACTORY_TYPE.D2D1_FACTORY_TYPE_SINGLE_THREADED, typeof(NativeMethods.ID2D1Factory).GUID, IntPtr.Zero, out var factory);
+                    if (hr < 0)
+                    {
+                        dpiX = 96;
+                    }
+                    else
+                    {
+                        factory.GetDesktopDpi(out var x, out _);
+                        Marshal.ReleaseComObject(factory);
+                        dpiX = (uint)x;
+                    }
+                }
+
+                this.ScaleFactor = dpiX / 96.0;
+            }
+
+            if (!MultiMonitorSupport || monitor == (IntPtr)PRIMARY_MONITOR)
+            {
+                var size = new Size(
+                    NativeMethods.GetSystemMetrics(NativeMethods.SystemMetric.SM_CXSCREEN),
+                    NativeMethods.GetSystemMetrics(NativeMethods.SystemMetric.SM_CYSCREEN));
+
+                this.Bounds = new Rect(0, 0, size.Width, size.Height);
+                this.Primary = true;
+                this.DeviceName = "DISPLAY";
+            }
+            else
+            {
+                var info = new NativeMethods.MONITORINFOEX();
+
+                NativeMethods.GetMonitorInfo(new HandleRef(null, monitor), info);
+
+                this.Bounds = new Rect(
+                    info.rcMonitor.left,
+                    info.rcMonitor.top,
+                    info.rcMonitor.right - info.rcMonitor.left,
+                    info.rcMonitor.bottom - info.rcMonitor.top);
+                this.Primary = (info.dwFlags & MONITORINFOF_PRIMARY) != 0;
+                this.DeviceName = new string(info.szDevice).TrimEnd((char)0);
+            }
+
+            this.monitorHandle = monitor;
+        }
+
+        /// <summary>
+        /// Gets an array of all displays on the system.
+        /// </summary>
+        /// <returns>An enumerable of type Screen, containing all displays on the system.</returns>
+        public static IEnumerable<ScreenHelper> AllScreens
+        {
+            get
+            {
+                if (MultiMonitorSupport)
+                {
+                    var closure = new MonitorEnumCallback();
+                    var proc = new NativeMethods.MonitorEnumProc(closure.Callback);
+                    NativeMethods.EnumDisplayMonitors(NativeMethods.NullHandleRef, null, proc, IntPtr.Zero);
+                    if (closure.Screens.Count > 0)
+                    {
+                        return closure.Screens.Cast<ScreenHelper>();
+                    }
+                }
+
+                return new[] { new ScreenHelper((IntPtr)PRIMARY_MONITOR) };
+            }
+        }
+
+        /// <summary>
+        /// Gets the primary display.
+        /// </summary>
+        /// <returns>The primary display.</returns>
+        public static ScreenHelper PrimaryScreen
+        {
+            get
+            {
+                return MultiMonitorSupport ? AllScreens.FirstOrDefault(t => t.Primary) : new ScreenHelper((IntPtr)PRIMARY_MONITOR);
+            }
+        }
+
+        /// <summary>
+        /// Gets the bounds of the display in units.
+        /// </summary>
+        /// <returns>A <see cref="T:System.Windows.Rect" />, representing the bounds of the display in units.</returns>
+        public Rect WpfBounds =>
+            this.ScaleFactor.Equals(1.0)
+                ? this.Bounds
+                : new Rect(
+                    this.Bounds.X / this.ScaleFactor,
+                    this.Bounds.Y / this.ScaleFactor,
+                    this.Bounds.Width / this.ScaleFactor,
+                    this.Bounds.Height / this.ScaleFactor);
+
+        /// <summary>
+        /// Gets the device name associated with a display.
+        /// </summary>
+        /// <returns>The device name associated with a display.</returns>
+        public string DeviceName { get; }
+
+        /// <summary>
+        /// Gets the bounds of the display in pixels.
+        /// </summary>
+        /// <returns>A <see cref="T:System.Windows.Rect" />, representing the bounds of the display in pixels.</returns>
+        public Rect Bounds { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether a particular display is the primary device.
+        /// </summary>
+        /// <returns>true if this display is primary; otherwise, false.</returns>
+        public bool Primary { get; }
+
+        /// <summary>
+        /// Gets the scale factor of the display.
+        /// </summary>
+        /// <returns>The scale factor of the display.</returns>
+        public double ScaleFactor { get; } = 1.0;
+
+        /// <summary>
+        /// Gets the working area of the display. The working area is the desktop area of the display, excluding task bars,
+        /// docked windows, and docked tool bars in pixels.
+        /// </summary>
+        /// <returns>A <see cref="T:System.Windows.Rect" />, representing the working area of the display in pixels.</returns>
+        public Rect WorkingArea
+        {
+            get
+            {
+                Rect workingArea;
+
+                if (!MultiMonitorSupport || this.monitorHandle == (IntPtr)PRIMARY_MONITOR)
+                {
+                    var rc = new NativeMethods.RECT();
+
+                    NativeMethods.SystemParametersInfo(NativeMethods.SPI.SPI_GETWORKAREA, 0, ref rc, NativeMethods.SPIF.SPIF_SENDCHANGE);
+
+                    workingArea = new Rect(rc.left, rc.top, rc.right - rc.left, rc.bottom - rc.top);
+                }
+                else
+                {
+                    var info = new NativeMethods.MONITORINFOEX();
+                    NativeMethods.GetMonitorInfo(new HandleRef(null, this.monitorHandle), info);
+
+                    workingArea = new Rect(info.rcWork.left, info.rcWork.top, info.rcWork.right - info.rcWork.left, info.rcWork.bottom - info.rcWork.top);
+                }
+
+                return workingArea;
+            }
+        }
+
+        /// <summary>
+        /// Gets the working area of the display. The working area is the desktop area of the display, excluding task bars,
+        /// docked windows, and docked tool bars in units.
+        /// </summary>
+        /// <returns>A <see cref="T:System.Windows.Rect" />, representing the working area of the display in units.</returns>
+        public Rect WpfWorkingArea =>
+            this.ScaleFactor.Equals(1.0)
+                ? this.WorkingArea
+                : new Rect(
+                    this.WorkingArea.X / this.ScaleFactor,
+                    this.WorkingArea.Y / this.ScaleFactor,
+                    this.WorkingArea.Width / this.ScaleFactor,
+                    this.WorkingArea.Height / this.ScaleFactor);
+
+        /// <summary>
+        /// Retrieves a Screen for the display that contains the largest portion of the specified control.
+        /// </summary>
+        /// <param name="hwnd">The window handle for which to retrieve the Screen.</param>
+        /// <returns>
+        /// A Screen for the display that contains the largest region of the object. In multiple display environments
+        /// where no display contains any portion of the specified window, the display closest to the object is returned.
+        /// </returns>
+        public static ScreenHelper FromHandle(IntPtr hwnd)
+        {
+            return MultiMonitorSupport
+                       ? new ScreenHelper(NativeMethods.MonitorFromWindow(new HandleRef(null, hwnd), 2))
+                       : new ScreenHelper((IntPtr)PRIMARY_MONITOR);
+        }
+
+        /// <summary>
+        /// Retrieves a Screen for the display that contains the specified point in pixels.
+        /// </summary>
+        /// <param name="point">A <see cref="T:System.Windows.Point" /> that specifies the location for which to retrieve a Screen.</param>
+        /// <returns>
+        /// A Screen for the display that contains the point in pixels. In multiple display environments where no display contains
+        /// the point, the display closest to the specified point is returned.
+        /// </returns>
+        public static ScreenHelper FromPoint(Point point)
+        {
+            if (MultiMonitorSupport)
+            {
+                var pt = new NativeMethods.POINTSTRUCT((int)point.X, (int)point.Y);
+                return new ScreenHelper(NativeMethods.MonitorFromPoint(pt, NativeMethods.MonitorDefault.MONITOR_DEFAULTTONEAREST));
+            }
+
+            return new ScreenHelper((IntPtr)PRIMARY_MONITOR);
+        }
+
+        /// <summary>
+        /// Retrieves a Screen for the display that contains the largest portion of the specified control.
+        /// </summary>
+        /// <param name="window">The window for which to retrieve the Screen.</param>
+        /// <returns>
+        /// A Screen for the display that contains the largest region of the object. In multiple display environments
+        /// where no display contains any portion of the specified window, the display closest to the object is returned.
+        /// </returns>
+        public static ScreenHelper FromWindow(Window window)
+        {
+            return FromHandle(new WindowInteropHelper(window).Handle);
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the specified object is equal to this Screen.
+        /// </summary>
+        /// <param name="obj">The object to compare to this Screen.</param>
+        /// <returns>true if the specified object is equal to this Screen; otherwise, false.</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is ScreenHelper monitor)
+            {
+                if (this.monitorHandle == monitor.monitorHandle)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Computes and retrieves a hash code for an object.
+        /// </summary>
+        /// <returns>A hash code for an object.</returns>
+        public override int GetHashCode()
+        {
+            return this.monitorHandle.GetHashCode();
+        }
+
+        /// <summary>
+        /// The monitor enum callback.
+        /// </summary>
+        private class MonitorEnumCallback
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="MonitorEnumCallback"/> class.
+            /// </summary>
+            public MonitorEnumCallback()
+            {
+                this.Screens = new ArrayList();
+            }
+
+            /// <summary>
+            /// Gets the screens.
+            /// </summary>
+            public ArrayList Screens { get; }
+
+            public bool Callback(IntPtr monitor, IntPtr hdc, IntPtr lprcMonitor, IntPtr lparam)
+            {
+                this.Screens.Add(new ScreenHelper(monitor, hdc));
+                return true;
+            }
+        }
+    }
+}
---
+++ WPF_TEST/NativeMethods.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Windows;
+
+namespace WPF_TEST
+{
+    internal static class NativeMethods
+    {
+        public delegate bool MonitorEnumProc(IntPtr monitor, IntPtr hdc, IntPtr lprcMonitor, IntPtr lParam);
+
+        public enum DpiType
+        {
+            EFFECTIVE = 0,
+            ANGULAR = 1,
+            RAW = 2
+        }
+
+        public enum SystemMetric
+        {
+            SM_CXSCREEN = 0,
+            SM_CYSCREEN = 1,
+            SM_XVIRTUALSCREEN = 76,
+            SM_YVIRTUALSCREEN = 77,
+            SM_CXVIRTUALSCREEN = 78,
+            SM_CYVIRTUALSCREEN = 79,
+            SM_CMONITORS = 80
+        }
+
+        public enum SPI : uint
+        {
+            /// <summary>
+            /// Retrieves the size of the work area on the primary display monitor. The work area is the portion of the screen not obscured
+            /// by the system taskbar or by application desktop toolbars. The pvParam parameter must point to a RECT structure that receives
+            /// the coordinates of the work area, expressed in virtual screen coordinates.
+            /// To get the work area of a monitor other than the primary display monitor, call the GetMonitorInfo function.
+            /// </summary>
+            SPI_GETWORKAREA = 0x0030
+        }
+
+        [Flags]
+        public enum SPIF
+        {
+            None = 0x00,
+
+            /// <summary>Writes the new system-wide parameter setting to the user profile.</summary>
+            SPIF_UPDATEINIFILE = 0x01,
+
+            /// <summary>Broadcasts the WM_SETTINGCHANGE message after updating the user profile.</summary>
+            SPIF_SENDCHANGE = 0x02,
+
+            /// <summary>Same as SPIF_SENDCHANGE.</summary>
+            SPIF_SENDWININICHANGE = 0x02
+        }
+
+        public enum MonitorDefault
+        {
+            /// <summary>If the point is not contained within any display monitor, return a handle to the display monitor that is nearest to the point.</summary>
+            MONITOR_DEFAULTTONEAREST = 0x00000002,
+
+            /// <summary>If the point is not contained within any display monitor, return NULL.</summary>
+            MONITOR_DEFAULTTONULL = 0x00000000,
+
+            /// <summary>If the point is not contained within any display monitor, return a handle to the primary display monitor.</summary>
+            MONITOR_DEFAULTTOPRIMARY = 0x00000001
+        }
+
+        public enum D2D1_FACTORY_TYPE
+        {
+            D2D1_FACTORY_TYPE_SINGLE_THREADED = 0,
+            D2D1_FACTORY_TYPE_MULTI_THREADED = 1,
+        }
+
+        public static readonly HandleRef NullHandleRef = new HandleRef(null, IntPtr.Zero);
+
+        [DllImport("shcore.dll", CharSet = CharSet.Auto)]
+        [ResourceExposure(ResourceScope.None)]
+        public static extern IntPtr GetDpiForMonitor([In] IntPtr hmonitor, [In] DpiType dpiType, [Out] out uint dpiX, [Out] out uint dpiY);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        [ResourceExposure(ResourceScope.None)]
+        public static extern bool GetMonitorInfo(HandleRef hmonitor, [In][Out] MONITORINFOEX info);
+
+        [DllImport("user32.dll", ExactSpelling = true)]
+        [ResourceExposure(ResourceScope.None)]
+        public static extern bool EnumDisplayMonitors(HandleRef hdc, COMRECT rcClip, MonitorEnumProc lpfnEnum, IntPtr dwData);
+
+        [DllImport("user32.dll", ExactSpelling = true)]
+        [ResourceExposure(ResourceScope.None)]
+        public static extern IntPtr MonitorFromWindow(HandleRef handle, int flags);
+
+        [DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
+        [ResourceExposure(ResourceScope.None)]
+        public static extern int GetSystemMetrics(SystemMetric nIndex);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        [ResourceExposure(ResourceScope.None)]
+        public static extern bool SystemParametersInfo(SPI nAction, int nParam, ref RECT rc, SPIF nUpdate);
+
+        [DllImport("user32.dll", ExactSpelling = true)]
+        [ResourceExposure(ResourceScope.None)]
+        public static extern IntPtr MonitorFromPoint(POINTSTRUCT pt, MonitorDefault flags);
+
+        [DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
+        [ResourceExposure(ResourceScope.None)]
+        public static extern bool GetCursorPos([In][Out] POINT pt);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool IsProcessDPIAware();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        [DllImport("d2d1.dll")]
+        public static extern int D2D1CreateFactory(D2D1_FACTORY_TYPE factoryType, [MarshalAs(UnmanagedType.LPStruct)] Guid riid, IntPtr pFactoryOptions, out ID2D1Factory ppIFactory);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong")]
+        public static extern int SetWindowLong32(HandleRef hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr")]
+        public static extern IntPtr SetWindowLongPtr64(HandleRef hWnd, int nIndex, IntPtr dwNewLong);
+
+        [DllImport("user32.dll")]
+        public static extern bool EnumDisplaySettings(string lpszDeviceName, int iModeNum, ref DEVMODE lpDevMode);
+
+        public static class WS
+        {
+            public static readonly long
+            WS_BORDER = 0x00800000L,
+            WS_CAPTION = 0x00C00000L,
+            WS_CHILD = 0x40000000L,
+            WS_CHILDWINDOW = 0x40000000L,
+            WS_CLIPCHILDREN = 0x02000000L,
+            WS_CLIPSIBLINGS = 0x04000000L,
+            WS_DISABLED = 0x08000000L,
+            WS_DLGFRAME = 0x00400000L,
+            WS_GROUP = 0x00020000L,
+            WS_HSCROLL = 0x00100000L,
+            WS_ICONIC = 0x20000000L,
+            WS_MAXIMIZE = 0x01000000L,
+            WS_MAXIMIZEBOX = 0x00010000L,
+            WS_MINIMIZE = 0x20000000L,
+            WS_MINIMIZEBOX = 0x00020000L,
+            WS_OVERLAPPED = 0x00000000L,
+            WS_OVERLAPPEDWINDOW = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX,
+            WS_POPUP = 0x80000000L,
+            WS_POPUPWINDOW = WS_POPUP | WS_BORDER | WS_SYSMENU,
+            WS_SIZEBOX = 0x00040000L,
+            WS_SYSMENU = 0x00080000L,
+            WS_TABSTOP = 0x00010000L,
+            WS_THICKFRAME = 0x00040000L,
+            WS_TILED = 0x00000000L,
+            WS_TILEDWINDOW = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX,
+            WS_VISIBLE = 0x10000000L,
+            WS_VSCROLL = 0x00200000L;
+        }
+
+        public static IntPtr SetWindowLongPtr(HandleRef hWnd, int nIndex, IntPtr dwNewLong)
+        {
+            if (IntPtr.Size == 8)
+            {
+                return NativeMethods.SetWindowLongPtr64(hWnd, nIndex, dwNewLong);
+            }
+            else
+            {
+                return new IntPtr(NativeMethods.SetWindowLong32(hWnd, nIndex, dwNewLong.ToInt32()));
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct DEVMODE
+        {
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x20)]
+            public string dmDeviceName;
+            public short dmSpecVersion;
+            public short dmDriverVersion;
+            public short dmSize;
+            public short dmDriverExtra;
+            public int dmFields;
+            public int dmPositionX;
+            public int dmPositionY;
+            public int dmDisplayOrientation;
+            public int dmDisplayFixedOutput;
+            public short dmColor;
+            public short dmDuplex;
+            public short dmYResolution;
+            public short dmTTOption;
+            public short dmCollate;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x20)]
+            public string dmFormName;
+            public short dmLogPixels;
+            public int dmBitsPerPel;
+            public int dmPelsWidth;
+            public int dmPelsHeight;
+            public int dmDisplayFlags;
+            public int dmDisplayFrequency;
+            public int dmICMMethod;
+            public int dmICMIntent;
+            public int dmMediaType;
+            public int dmDitherType;
+            public int dmReserved1;
+            public int dmReserved2;
+            public int dmPanningWidth;
+            public int dmPanningHeight;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+
+            public RECT(int left, int top, int right, int bottom)
+            {
+                this.left = left;
+                this.top = top;
+                this.right = right;
+                this.bottom = bottom;
+            }
+
+            public RECT(Rect r)
+            {
+                left = (int)r.Left;
+                top = (int)r.Top;
+                right = (int)r.Right;
+                bottom = (int)r.Bottom;
+            }
+
+            public static RECT FromXYWH(int x, int y, int width, int height)
+            {
+                return new RECT(x, y, x + width, y + height);
+            }
+
+            public Size Size => new Size(right - left, bottom - top);
+        }
+
+        // use this in cases where the Native API takes a POINT not a POINT*
+        // classes marshal by ref.
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINTSTRUCT
+        {
+            public int x;
+            public int y;
+
+            public POINTSTRUCT(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public class POINT
+        {
+            public int x;
+            public int y;
+
+            public POINT()
+            {
+            }
+
+            public POINT(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+#if DEBUG
+
+            public override string ToString()
+            {
+                return "{x=" + x + ", y=" + y + "}";
+            }
+
+#endif
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto, Pack = 4)]
+        public class MONITORINFOEX
+        {
+            internal int cbSize = Marshal.SizeOf(typeof(MONITORINFOEX));
+
+            internal RECT rcMonitor = new RECT();
+            internal RECT rcWork = new RECT();
+            internal int dwFlags = 0;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)]
+            internal char[] szDevice = new char[32];
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public class COMRECT
+        {
+            public int bottom;
+            public int left;
+            public int right;
+            public int top;
+
+            public COMRECT()
+            {
+            }
+
+            public COMRECT(Rect r)
+            {
+                left = (int)r.X;
+                top = (int)r.Y;
+                right = (int)r.Right;
+                bottom = (int)r.Bottom;
+            }
+
+            public COMRECT(int left, int top, int right, int bottom)
+            {
+                this.left = left;
+                this.top = top;
+                this.right = right;
+                this.bottom = bottom;
+            }
+
+            public static COMRECT FromXYWH(int x, int y, int width, int height)
+            {
+                return new COMRECT(x, y, x + width, y + height);
+            }
+
+            public override string ToString()
+            {
+                return "Left = " + left + " Top " + top + " Right = " + right + " Bottom = " + bottom;
+            }
+        }
+
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("06152247-6f50-465a-9245-118bfd3b6007")]
+        internal interface ID2D1Factory
+        {
+            int ReloadSystemMetrics();
+
+            [PreserveSig]
+            void GetDesktopDpi(out float dpiX, out float dpiY);
+
+            // the rest is not implemented as we don't need it
+        }
+    }
+}
